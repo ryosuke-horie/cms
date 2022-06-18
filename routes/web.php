1c2d3ce66cf2c799f5c3ec29f6c3e118ad02a45f
EC2 Default User <ec2-user@ip-172-31-24-252.us-west-1.compute.internal>
2022-06-18 13:37:15 +0000

    ルーティングを作成

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,26 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Book;
+use Illuminate\Http\Request;
 
+/**
+ * 本の一覧表示
+ */
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+/**
+ * 本を追加
+ */
+ Route::post('book', function (Request $request) {
+    // 
+ });
+ 
+ /**
+  * 本を削除
+  */
+  Route::delete('/book/{book}', function(Book $book) {
+      //
+  });
