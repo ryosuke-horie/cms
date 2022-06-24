57969a719401c4831f2f927eae7d1570c59cfb4a
EC2 Default User <ec2-user@ip-172-31-24-252.us-west-1.compute.internal>
2022-06-24 01:04:37 +0000

    削除処理の移植

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,22 @@
 <?php
-
 use App\Book;
-use Illuminate\Http\Request;
+use Illuminate\Http\Request; 
 
-
-/**
-* 本ダッシュボート表示(books.blade.php)
-*/
+//本ダッシュボード表示
 Route::get('/', 'BooksController@index');
 
-/**
-* 本を登録
-*/
-Route::post('/books', 'BooksController@store');
+//登録処理
+Route::post('/books','BooksController@store');
 
 //更新画面
 Route::post('/booksedit/{books}','BooksController@edit');
 
 //更新処理
-Route::post('/books/update', 'BooksController@update');
+Route::post('/books/update','BooksController@update');
 
-/**
-* 本を削除 
-*/
-Route::delete('/book/{book}', function (Book $book) {
-    $book->delete();
-    return redirect('/');
-});
+//本を削除
+Route::delete('/book/{book}','BooksController@destroy');
 
+//Auth
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
